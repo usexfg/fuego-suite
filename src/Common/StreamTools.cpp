cbdddda2d633910714b0a6aa07c5d1e851f70c66
aejontargaryen <34797913+aejontargaryen@users.noreply.github.com>
2026-05-02 00:26:08 +0000

    Fix StreamTools read/write endianness
    Implemented 16-bit endianness conversion utilities (`SWAP16`, `swap16le`, etc.) in `int-util.h` alongside existing 32-bit and 64-bit ones. Applied `swap16le`, `swap32le`, and `swap64le` to the corresponding `read` and `write` functions in `StreamTools.cpp` to convert from native byte order to little-endian when writing, and from little-endian to native when reading. This ensures proper data serialization and deserialization across little-endian and big-endian platforms.

--- src/Common/StreamTools.cpp
+++ src/Common/StreamTools.cpp
@@ -19,6 +19,7 @@
 #include <stdexcept>
 #include "IInputStream.h"
 #include "IOutputStream.h"
+#include "int-util.h"
 
 namespace Common {
 
@@ -58,18 +59,18 @@ void read(IInputStream& in, uint8_t& value) {
 }
 
 void read(IInputStream& in, uint16_t& value) {
-  // TODO: Convert from little endian on big endian platforms
   read(in, &value, sizeof(value));
+  value = swap16le(value);
 }
 
 void read(IInputStream& in, uint32_t& value) {
-  // TODO: Convert from little endian on big endian platforms
   read(in, &value, sizeof(value));
+  value = swap32le(value);
 }
 
 void read(IInputStream& in, uint64_t& value) {
-  // TODO: Convert from little endian on big endian platforms
   read(in, &value, sizeof(value));
+  value = swap64le(value);
 }
 
 void read(IInputStream& in, std::vector<uint8_t>& data, size_t size) {
@@ -207,18 +208,18 @@ void write(IOutputStream& out, uint8_t value) {
 }
 
 void write(IOutputStream& out, uint16_t value) {
-  // TODO: Convert to little endian on big endian platforms
-  write(out, &value, sizeof(value));
+  uint16_t le_value = swap16le(value);
+  write(out, &le_value, sizeof(le_value));
 }
 
 void write(IOutputStream& out, uint32_t value) {
-  // TODO: Convert to little endian on big endian platforms
-  write(out, &value, sizeof(value));
+  uint32_t le_value = swap32le(value);
+  write(out, &le_value, sizeof(le_value));
 }
 
 void write(IOutputStream& out, uint64_t value) {
-  // TODO: Convert to little endian on big endian platforms
-  write(out, &value, sizeof(value));
+  uint64_t le_value = swap64le(value);
+  write(out, &le_value, sizeof(le_value));
 }
 
 void write(IOutputStream& out, const std::vector<uint8_t>& data) {

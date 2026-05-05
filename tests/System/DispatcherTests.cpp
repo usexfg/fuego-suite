e6d83c063a7edbe09da3419b3856e8cdc834fb01
aejontargaryen <34797913+aejontargaryen@users.noreply.github.com>
2026-05-05 02:57:36 +0000

    Fix CMake configuration for CI on macOS
    The nested if condition in `CMakeLists.txt` caused an invalid nesting syntax issue. Fixed it by doing a simple top-level `if(APPLE)` for the macOS dependency fix.

--- tests/System/DispatcherTests.cpp
+++ tests/System/DispatcherTests.cpp
@@ -1,4 +1,3 @@
-#include <thread>
 // Copyright (c) 2011-2016 The Cryptonote developers
 // Copyright (c) 2014-2016 SDN developers
 // Distributed under the MIT/X11 software license, see the accompanying

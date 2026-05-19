e68f24e4748f8f251eaf549f3fed1a114ad22a70
ColinRitman <aejontargaryen@pm.me>
2026-05-19 08:14:10 -0400

    revert: v10 uses parent-block hashing blob (same as v2-v9)

--- src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
@@ -566,12 +566,8 @@ bool get_block_longhash(cn_context &context, const Block& b, Hash& res) {
     if (!get_block_hashing_blob(b, bd)) {
       return false;
     }
-  } else if (b.majorVersion >= BLOCK_MAJOR_VERSION_2 && b.majorVersion <= BLOCK_MAJOR_VERSION_9) {
+  } else if (b.majorVersion >= BLOCK_MAJOR_VERSION_2) {
     if (!get_parent_block_hashing_blob(b, bd)) {
-      return false;
-    }
-  } else if (b.majorVersion >= BLOCK_MAJOR_VERSION_10) {
-    if (!get_block_hashing_blob(b, bd)) {
       return false;
     }
   } else {
